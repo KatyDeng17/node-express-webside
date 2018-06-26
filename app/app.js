42a4b9ba62dc677ab1b781e4f0f0af6d2d870a93
Katy Deng <deng2017.xq@gmail.com>
2018-06-25 23:27:19 -0700

    adding routing

--- app/app.js
+++ app/app.js
@@ -6,25 +6,41 @@ app.set('port', process.env.PORT ||3000); // setting default to 3000 when user d
 
 
 app.get('/',(req, res)=>{
-    let info = ''; 
-    dataFile.speakers.forEach((item)=>{
-      info += `
-       <li> 
-         <h2>${item.name}</h2> 
-         <p>${item.summary}</p>
-      `;
-    });
-
     res.send(`
-       <h1> Roux Academy Meetup</h1>
-       ${info}
+       <h1>Welcome</h1>
+       <p>Roux Academy Meetups put together artists from all walks of life</p>
+ 
     `);
 });
-const server = app.listen(app.get('port'), ()=>{
-  console.log('Express Server running on Localhost: ' + app.get('port'));
+
+app.get('/speakers/:speakerid',(req, res)=>{
+  let speaker = dataFile.speakers[req.params.speakerid];
+  res.send(`
+     <h1> ${speaker.title}</h1>
+     <h2> ${speaker.name}</h2>
+     <p> ${speaker.summary}</p>
+  `);
 });
 
+app.get('/speakers',(req, res)=>{
+  let info = ''; 
+  dataFile.speakers.forEach((item)=>{
+    info += `
+     <li> 
+       <h2>${item.name}</h2> 
+       <p>${item.summary}</p>
+    `;
+  });
 
+  res.send(`
+     <h1> Roux Academy Meetup</h1>
+     ${info}
+  `);
+});
+
+const server = app.listen(app.get('port'), ()=>{
+console.log('Express Server running on Localhost: ' + app.get('port'));
+})
 
 // const http = require('http');
 // const server = http.createServer((req, res)=>{
